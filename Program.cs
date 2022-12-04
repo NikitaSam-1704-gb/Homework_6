5cabe36e87e3dc2141e1c38d8378d0abc6637dd3
Никита <samnikita141204@yandex.ru>
2022-12-04 16:08:37 +0400

    Задача 44 ряд фибоначи

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ if(summ-sideMaх>sideMaх)
 else
     WriteLine(" Сданными значениями сторон треугольник построить НЕЛЬЗЯ");*/
 
-// Задача 42 напишите программу преодразования десятичного числа в двоичное
+/*// Задача 42 напишите программу преодразования десятичного числа в двоичное
 // 45 -> 101101
 // 3-> 11
 // 2-> 10 
@@ -82,4 +82,46 @@ String text=(" Введите число ");
 int chisloWork=IntFromConsole(text);
 Write($" Число   {chisloWork}  в двоичной системе -> ");
 string codeBinary=ConvertingNumberDecimalBinary(chisloWork);
-WriteLine(codeBinary);
+WriteLine(codeBinary);*/
+
+// Не используя рекурсию выведите первые N чисел Фибоначи
+// Если N=5 -> 0 1 1 2 3
+// Если N=7 -> 0 1 1 2 3 5 8
+
+using static System.Console;
+Clear();
+
+ int IntFromConsole( string message)
+ {
+    Write(message);
+    int number=Convert.ToInt16(ReadLine());
+    return number;
+ }
+
+ string FibonacciSeries(int number)
+ {
+    string fibonacci="";
+    int[] summ=new int[number+1];
+    summ[0]=0;
+    summ[1]=1;
+    if(number>=2)
+        fibonacci=fibonacci+summ[0]+summ[1];
+    else
+    {
+        fibonacci=fibonacci+summ[0];
+        return fibonacci;
+    }
+
+    
+    for(int i=2; i<number; i++)
+    {
+        summ[i]=summ[i-1]+summ[i-2];
+        fibonacci=fibonacci+summ[i];
+    }
+    return fibonacci;
+ }
+
+String text=(" Введите число элементов N-> ");
+int numberWork=IntFromConsole(text);
+string series=FibonacciSeries(numberWork);
+WriteLine($" ряд Фибоначи для {numberWork} элементов {series}");
