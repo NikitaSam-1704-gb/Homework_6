5a08e32eb968e4339cdac5fe13cd9572f68eb152
Никита <samnikita141204@yandex.ru>
2022-12-05 01:37:06 +0400

    Ветка master

--- Program.cs
+++ Program.cs
@@ -169,7 +169,7 @@ string strWork=IntFromConsole(text);
 int countWork=CountPositive(strWork);
 WriteLine($"Количество положительных элементов - > {countWork}");*/
 
-//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+/*//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
@@ -200,4 +200,4 @@ WriteLine($" Уравнение прямой №2 {line2.a2}x+{line2.b2}y+{line2
 
 double y= (line2.a2*line1.c1-line1.a1*line2.c2)/(line2.b2*line1.a1-line1.b1*line2.a2);
 double x=(-line1.c1/line1.a1)-(line1.b1/line1.a1)*y;
-WriteLine($" Кординаты точки пересечения ({x},{y})");
+WriteLine($" Кординаты точки пересечения ({x},{y})");*/
